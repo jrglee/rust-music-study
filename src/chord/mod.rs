8d51f3f6fe21915bc51de5c72c0c9adb4812ad0e
Jorge Lee <jrg.lee@gmail.com>
2022-02-09 12:08:33 -0800

    Major and minor chords

--- src/chord/mod.rs
+++ src/chord/mod.rs
@@ -1,2 +1,33 @@
+use triad::Triad;
+
+use crate::note::Note;
+
 pub mod tetrad;
 pub mod triad;
+
+pub fn major(root: &Note) -> [Note; 3] {
+    Triad::Major.intervals().map(|int| int.apply_to_note(root))
+}
+
+pub fn minor(root: &Note) -> [Note; 3] {
+    Triad::Minor.intervals().map(|int| int.apply_to_note(root))
+}
+
+#[cfg(test)]
+mod tests {
+    use Note::*;
+
+    use super::*;
+
+    #[test]
+    fn major_chord() {
+        assert_eq!(major(&C), [C, E, G]);
+        assert_eq!(major(&G), [G, B, D]);
+    }
+
+    #[test]
+    fn minor_chord() {
+        assert_eq!(minor(&C), [C, Eb, G]);
+        assert_eq!(minor(&A), [A, C, E]);
+    }
+}
